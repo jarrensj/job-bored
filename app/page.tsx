5ae136c91a4b6ddb5674a48eab9bd6df767a09c2
leoli7405@gmail.com <leoli7405@gmail.com>
2025-08-06 18:28:11 -0700

    landing page done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
@@ -20,6 +29,19 @@ export default function Home() {
         </div>
       </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className={"intro"}>
+          <h1>FIND YOUR DREAM TECH JOB</h1>
+          <p>Join thousands who’ve found jobs with our help! Our advanced AI technology matches you with opportunities tailored to your unique skills.</p>
+          <div className={"intro-card"}>
+            <Card className="w-full max-w-sm">
+              <CardContent>
+                <CardDescription>
+                  Sigin in to search for jobs and save your preferences :D
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
